cecf4928ccffb5780edefd346b208d2c249c2bcb
SImme PC <99250621+Urban-catastrofe@users.noreply.github.com>
2025-04-01 22:43:59 +0200

    Betalingscontroller

--- simme/Applicatie/src/main/java/Triptop/Applicatie/controller/BetalingController.java
+++ simme/Applicatie/src/main/java/Triptop/Applicatie/controller/BetalingController.java
@@ -1,8 +1,33 @@
 package Triptop.Applicatie.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import Triptop.Applicatie.dto.betaling.BetalingsResultaat;
+import Triptop.Applicatie.dto.betaling.BetalingsVerzoek;
+import lombok.AllArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import Triptop.Applicatie.model.betaling.Betaling;
+import Triptop.Applicatie.service.BetalingService;
 
 @RestController
+@AllArgsConstructor
+@RequestMapping("/betaling")
 public class BetalingController {
-    
+    private final BetalingService betalingService;
+
+    @PostMapping("/add")
+    public BetalingsResultaat verwerkBetaling(@RequestBody BetalingsVerzoek betaling) {
+        return betalingService.verwerkBetaling(betaling);
+    }
+
+    @GetMapping("/status")
+    public Betaling controleerStatus(@RequestParam String betalingId) {
+        return betalingService.haalBetalingOp(betalingId);
+    }
 }
