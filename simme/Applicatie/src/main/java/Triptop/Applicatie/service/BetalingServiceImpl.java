3232d7970525d6a89dfb20b7668ddf7eb703e7ec
SImme PC <99250621+Urban-catastrofe@users.noreply.github.com>
2025-04-01 22:50:49 +0200

    initial setup for the betalingsfactory, need to create the repository logic tummorow

--- simme/Applicatie/src/main/java/Triptop/Applicatie/service/BetalingServiceImpl.java
+++ simme/Applicatie/src/main/java/Triptop/Applicatie/service/BetalingServiceImpl.java
@@ -4,21 +4,28 @@ import org.springframework.stereotype.Service;
 
 import Triptop.Applicatie.dto.betaling.BetalingsResultaat;
 import Triptop.Applicatie.dto.betaling.BetalingsVerzoek;
+import Triptop.Applicatie.factory.betaling.BetalingFactory;
 import Triptop.Applicatie.model.betaling.Betaling;
 
+import lombok.AllArgsConstructor;
+
+import Triptop.Applicatie.factory.betaling.BetalingAdapter;
+
 @Service
+@AllArgsConstructor
 public class BetalingServiceImpl implements BetalingService {
+    private final BetalingFactory betalingFactory;
 
     @Override
     public BetalingsResultaat verwerkBetaling(BetalingsVerzoek betaling) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'verwerkBetaling'");
+        BetalingAdapter betalingAdapter = betalingFactory.createBetalingAdapter(betaling.getMethode());
+        return betalingAdapter.verwerkBetaling(betaling);
     }
 
     @Override
     public Betaling haalBetalingOp(String betalingId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'haalBetalingOp'");
+        BetalingAdapter betalingAdapter = betalingFactory.createBetalingAdapter(betaling.getMethode());
+        return betalingAdapter.controleerStatus(betalingId);
     }
 
 }
