d722919353c271cf61acab6c90739713aa32acc0
SImme PC <99250621+Urban-catastrofe@users.noreply.github.com>
2025-04-01 22:29:09 +0200

    added default methods to layout the structure of the service

--- simme/Applicatie/src/main/java/Triptop/Applicatie/service/BetalingServiceImpl.java
+++ simme/Applicatie/src/main/java/Triptop/Applicatie/service/BetalingServiceImpl.java
@@ -1,6 +1,24 @@
 package Triptop.Applicatie.service;
 
+import org.springframework.stereotype.Service;
+
+import Triptop.Applicatie.dto.betaling.BetalingsResultaat;
+import Triptop.Applicatie.dto.betaling.BetalingsVerzoek;
+import Triptop.Applicatie.model.betaling.Betaling;
+
+@Service
 public class BetalingServiceImpl implements BetalingService {
 
+    @Override
+    public BetalingsResultaat verwerkBetaling(BetalingsVerzoek betaling) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'verwerkBetaling'");
+    }
+
+    @Override
+    public Betaling haalBetalingOp(String betalingId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'haalBetalingOp'");
+    }
 
 }
