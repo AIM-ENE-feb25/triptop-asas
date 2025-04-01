2781e1b7cc417f332d36ee48bb746cd7abb96a25
SImme PC <99250621+Urban-catastrofe@users.noreply.github.com>
2025-04-01 22:20:15 +0200

    added betalingservice

--- simme/Applicatie/src/main/java/Triptop/Applicatie/service/BetalingService.java
+++ simme/Applicatie/src/main/java/Triptop/Applicatie/service/BetalingService.java
@@ -1,6 +1,10 @@
 package Triptop.Applicatie.service;
 
-public interface BetalingService {
-    
-}
+import Triptop.Applicatie.model.Betaling;
+import Triptop.Applicatie.model.BetalingsResultaat;
 
+public interface BetalingService {
+    public BetalingsResultaat verwerkBetaling(BetalingsVerzoek betaling);
+
+    public Betaling haalBetalingOp(String betalingId);
+}
