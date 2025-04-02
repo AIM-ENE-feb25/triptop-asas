dfafd5959d2aefdd71fc9c95c9ecf4932d8b3d7c
aminebermo <aminebermo@hotmail.com>
2025-04-02 22:34:02 +0200

    prepared StripeAdapter for implementation

--- software-guidebook/Prototype Amine/Prototype Payment system/src/main/java/org/example/prototypepaymentsystem/adapter/StripeAdapter.java
+++ software-guidebook/Prototype Amine/Prototype Payment system/src/main/java/org/example/prototypepaymentsystem/adapter/StripeAdapter.java
@@ -1,6 +1,19 @@
 package org.example.prototypepaymentsystem.adapter;
 
+import org.example.prototypepaymentsystem.dto.PaymentDTO;
+import org.springframework.stereotype.Component;
+
+@Component
 public class StripeAdapter implements PaymentPort {
+    @Override
+    public void createPayment(PaymentDTO paymentDTO) {
+
+    }
+
+    @Override
+    public void confirmPayment(String paymentId) {
+
+    }
 
 
 }
