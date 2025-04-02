557849da472d796c26722a8e927f78223be6cc6e
aminebermo <aminebermo@hotmail.com>
2025-04-02 22:33:36 +0200

    implemented paymentservice

--- software-guidebook/Prototype Amine/Prototype Payment system/src/main/java/org/example/prototypepaymentsystem/service/PaymentService.java
+++ software-guidebook/Prototype Amine/Prototype Payment system/src/main/java/org/example/prototypepaymentsystem/service/PaymentService.java
@@ -1,6 +1,9 @@
 package org.example.prototypepaymentsystem.service;
 
+import org.example.prototypepaymentsystem.adapter.PaypalAdapter;
+import org.example.prototypepaymentsystem.adapter.StripeAdapter;
 import org.example.prototypepaymentsystem.dto.PaymentDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +12,32 @@ public class PaymentService {
     @Value("${payment.method}")
     private String paymentMethod;
 
+    @Autowired
+    private PaypalAdapter paypalAdapter;
+
+    @Autowired
+    private StripeAdapter stripeAdapter;
+
     public void processPayment(PaymentDTO payment) {
         switch (paymentMethod.toLowerCase()) {
             case "stripe":
-                System.out.println("Processing payment with Stripe");
+                stripeAdapter.createPayment(payment);
                 break;
             case "paypal":
-                System.out.println("Processing payment with PayPal");
+                paypalAdapter.createPayment(payment);
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported payment method: " + paymentMethod);
+        }
+    }
+
+    public void confirmPayment(String paymentId) {
+        switch (paymentMethod.toLowerCase()) {
+            case "stripe":
+                stripeAdapter.confirmPayment(paymentId);
+                break;
+            case "paypal":
+                paypalAdapter.confirmPayment(paymentId);
                 break;
             default:
                 throw new IllegalArgumentException("Unsupported payment method: " + paymentMethod);
