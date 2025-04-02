e5f9290cf6631e2974d1fa289b4363ad964f9523
aminebermo <aminebermo@hotmail.com>
2025-04-02 18:15:12 +0200

    Ontwikkelomgeving opgezet

---
+++ software-guidebook/Prototype Amine/Prototype betalen/src/main/java/org/example/prototypebetalen/PrototypeBetalenApplication.java
@@ -0,0 +1,13 @@
+package org.example.prototypebetalen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeBetalenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrototypeBetalenApplication.class, args);
+    }
+
+}
---
+++ software-guidebook/Prototype Amine/Prototype betalen/src/test/java/org/example/prototypebetalen/PrototypeBetalenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.prototypebetalen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeBetalenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
